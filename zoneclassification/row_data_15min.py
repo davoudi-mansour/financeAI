dcc2e37c80c6ed527a6147e6b3a087626fe3624b
mansour davoudi <davudimansur@gmail.com>
2024-04-13 22:59:41 +0330

    minor change on row_data_15min.py

--- zoneclassification/row_data_15min.py
+++ zoneclassification/row_data_15min.py
@@ -7,31 +7,40 @@ df_5min = pd.read_csv("data/EURJPY_5 Mins_Bid_2003.08.04_2024.04.13.csv")
 df_15min['Time (EET)'] = pd.to_datetime(df_15min['Time (EET)'])
 df_5min['Time (EET)'] = pd.to_datetime(df_5min['Time (EET)'])
 
-df_15min.set_index('Time (EET)', inplace=True)
-df_5min.set_index('Time (EET)', inplace=True)
-
 _15min_samples = []
 
-for idx, _15min_row in df_15min.iterrows():
-    corresponding_5min_rows = df_5min.loc[idx:idx + pd.Timedelta(minutes=15)]
-    print(f"For 15min row with date: {idx}, High: {_15min_row['High']}, Low: {_15min_row['Low']}")
-    _15min_high = _15min_row["High"]
-    _15min_low = _15min_row["Low"]
-    _15min_close = _15min_row["Close"]
+total_len = df_15min.shape[0]
+for idx in range(total_len-2):
 
-    print(corresponding_5min_rows, end="\n")
-    for idx, _5min_row in corresponding_5min_rows.iterrows():
-        _5min_high = _5min_row["High"]
-        _5min_low = _5min_row["Low"]
-        if _15min_high == _5min_high:
-            _15min_samples.append(_15min_high)
-            _15min_samples.append(_15min_low)
-            _15min_samples.append(_15min_close)
-            break
-        elif _15min_low == _5min_low:
-            _15min_samples.append(_15min_low)
-            _15min_samples.append(_15min_high)
-            _15min_samples.append(_15min_close)
-            break
+    first_row = df_15min.iloc[idx]
+    second_row = df_15min.iloc[idx+1]
+    timestamp_15min_first = df_15min.iloc[idx]['Time (EET)']
+    timestamp_15min_second = df_15min.iloc[idx+1]['Time (EET)']
+
+    corresponding_5min_rows = df_5min[
+        (df_5min['Time (EET)'] >= timestamp_15min_first) &
+        (df_5min['Time (EET)'] <= timestamp_15min_second)
+        ]
+    if not corresponding_5min_rows.empty:
+
+        #print(f"For 15min row between dates: {first_row['Time (EET)']} {second_row['Time (EET)']}, High: {second_row['High']}, Low: {second_row['Low']}")
+        _15min_high = second_row["High"]
+        _15min_low = second_row["Low"]
+        _15min_close = second_row["Close"]
+        #print(corresponding_5min_rows, end="\n")
+        for idx, _5min_row in corresponding_5min_rows.iterrows():
+            _5min_high = _5min_row["High"]
+            _5min_low = _5min_row["Low"]
+            if _15min_high == _5min_high:
+                _15min_samples.append(_15min_high)
+                _15min_samples.append(_15min_low)
+                _15min_samples.append(_15min_close)
+                break
+            elif _15min_low == _5min_low:
+                _15min_samples.append(_15min_low)
+                _15min_samples.append(_15min_high)
+                _15min_samples.append(_15min_close)
+                break
+
 
 np.save('data/_15min_samples.npy', _15min_samples)
